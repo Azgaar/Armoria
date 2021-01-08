95521f4cd44952e08ddf26269f68532f252707e2
Azgaar <maxganiev@yandex.ru>
2021-01-08 17:25:26 +0300

    v0.69a

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import WindowEvents from './WindowEvents.svelte';
   import Navbar from './Navbar.svelte';
   import About from './About.svelte';
   import Editor from './Editor.svelte';
@@ -10,7 +11,6 @@
   import Patterns from './Patterns.svelte';
   import {generate} from './generator.js';
   import {background, size, history, matrices, matrix, state, changes, message} from './stores.js';
-  import {download} from './download.js';
 
   let n, w, h, gallery = [], coa, seed;
   $: [n, w, h] = defineGallerySize($size);
@@ -90,38 +90,9 @@
     const h = Math.floor(height / numberY);
     return [numberX * numberY, w, h];
   }
-
-  // keyboard shortcuts
-  function handleKeydown(event) {
-    const code = event.code;
-    const reserved = ["Backspace", "Enter", "KeyZ", "KeyX", "KeyD", "F1", "Escape"];
-    if (!reserved.includes(code)) return;
-
-    const active = document.activeElement.tagName;
-    if (active === "INPUT" || active === "SELECT" || active === "TEXTAREA") return;
-
-    event.preventDefault();
-    if (code === "Backspace" && $matrix > 0) $matrix -= 1; else // Rollback
-    if (code === "Enter") $matrix += 1; else // Reroll
-    if (code === "KeyZ") changes.undo(); else // Undo
-    if (code === "KeyX") changes.redo(); else // Redo
-    if (code === "KeyD") download(); else // Download
-    if (code === "F1") $state.about = !$state.about; // About
-    if (code === "Escape") close() // Close Windows
-
-    function close() {
-      $state.about = 0;
-      $state.raster = 0;
-      $state.vector = 0;
-      $state.tinctures = 0;
-      $state.edit = 0;
-      $message = null;
-    }
-  }
 </script>
 
-<svelte:window on:keydown={handleKeydown}/>
-
+<WindowEvents/>
 <main style="background-color: {$background}">
   <Navbar/>
   {#if $state.about}<About/>{/if}
@@ -131,11 +102,7 @@
   {#if $state.edit}<Editor {coa} c={$state.c}/>
   {:else}<Gallery {gallery} {w} {h}/>{/if}
   {#if $message}<Message/>{/if}
-  <div id="patterns" style="position: absolute">
-    <svg width=0 height=0 xmlns="http://www.w3.org/2000/svg">
-      <defs><Patterns/></defs>
-    </svg>
-  </div>
+  <Patterns/>
 </main>
 
 <style>
@@ -143,4 +110,3 @@
     background-image: url("data:image/svg+xml,%3Csvg width='84' height='84' viewBox='0 0 84 84' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none'%3E%3Cg fill='%23222' fill-opacity='.2'%3E%3Cpath d='M84 23c-4.417 0-8-3.584-8-7.998V8h-7.002C64.58 8 61 4.42 61 0H23c0 4.417-3.584 8-7.998 8H8v7.002C8 19.42 4.42 23 0 23v38c4.417 0 8 3.584 8 7.998V76h7.002C19.42 76 23 79.58 23 84h38c0-4.417 3.584-8 7.998-8H76v-7.002C76 64.58 79.58 61 84 61V23zM59.05 83H43V66.95c5.054-.5 9-4.764 9-9.948V52h5.002c5.18 0 9.446-3.947 9.95-9H83v16.05c-5.054.5-9 4.764-9 9.948V74h-5.002c-5.18 0-9.446 3.947-9.95 9zm-34.1 0H41V66.95c-5.053-.502-9-4.768-9-9.948V52h-5.002c-5.184 0-9.447-3.946-9.95-9H1v16.05c5.053.502 9 4.768 9 9.948V74h5.002c5.184 0 9.447 3.946 9.95 9zm0-82H41v16.05c-5.054.5-9 4.764-9 9.948V32h-5.002c-5.18 0-9.446 3.947-9.95 9H1V24.95c5.054-.5 9-4.764 9-9.948V10h5.002c5.18 0 9.446-3.947 9.95-9zm34.1 0H43v16.05c5.053.502 9 4.768 9 9.948V32h5.002c5.184 0 9.447 3.946 9.95 9H83V24.95c-5.053-.502-9-4.768-9-9.948V10h-5.002c-5.184 0-9.447-3.946-9.95-9zM50 50v7.002C50 61.42 46.42 65 42 65c-4.417 0-8-3.584-8-7.998V50h-7.002C22.58 50 19 46.42 19 42c0-4.417 3.584-8 7.998-8H34v-7.002C34 22.58 37.58 19 42 19c4.417 0 8 3.584 8 7.998V34h7.002C61.42 34 65 37.58 65 42c0 4.417-3.584 8-7.998 8H50z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
   }
 </style>
-
---
+++ src/WindowEvents.svelte
@@ -0,0 +1,71 @@
+<script>
+  import {state, matrix, changes, message} from './stores.js';
+  import {download} from './download.js';
+  const touch = {startX:0, startY: 0};
+
+  // keyboard shortcuts
+  function handleKeydown(event) {
+    const code = event.code;
+    const reserved = ["Backspace", "Enter", "KeyZ", "KeyX", "KeyD", "F1", "Escape"];
+    if (!reserved.includes(code)) return;
+
+    const active = document.activeElement.tagName;
+    if (active === "INPUT" || active === "SELECT" || active === "TEXTAREA") return;
+
+    event.preventDefault();
+    if (code === "Backspace" && $matrix > 0) $matrix -= 1; else // Rollback
+    if (code === "Enter") $matrix += 1; else // Reroll
+    if (code === "KeyZ") changes.undo(); else // Undo
+    if (code === "KeyX") changes.redo(); else // Redo
+    if (code === "KeyD") download(); else // Download
+    if (code === "F1") $state.about = !$state.about; // About
+    if (code === "Escape") close() // Close all windows
+
+    function close() {
+      $state.about = 0;
+      $state.raster = 0;
+      $state.vector = 0;
+      $state.tinctures = 0;
+      $state.edit = 0;
+      $message = null;
+    }
+  }
+
+  function handleTouchStart(e) {
+    touch.startX = e.changedTouches[0].screenX;
+    touch.startY = e.changedTouches[0].screenY;
+  }
+
+  function handleTouchEnd(e) {
+    const diffX = e.changedTouches[0].screenX - touch.startX;
+    const diffY = e.changedTouches[0].screenY - touch.startY;
+    const ratioX = Math.abs(diffX / diffY);
+    const ratioY = Math.abs(diffY / diffX);
+    const absDiff = Math.abs(ratioX > ratioY ? diffX : diffY);
+
+    if (absDiff < 30) return; // ignore small movements
+
+    if (ratioX > ratioY) diffX >= 0 ? swipeRight() : swipeLeft();
+    else diffY >= 0 ? swipeDown() : swipeUp();
+  }
+
+  function swipeRight() {
+    if ($state.edit) changes.redo();
+    else $matrix += 1;
+  }
+
+  function swipeDown() {
+    $matrix += 1;
+  }
+
+  function swipeLeft() {
+    if ($state.edit) changes.undo();
+    else if ($matrix > 0) $matrix -= 1;
+  }
+
+  function swipeUp() {
+    if ($matrix > 0) $matrix -= 1;
+  }
+</script>
+
+<svelte:window on:keydown={handleKeydown} on:touchstart={handleTouchStart} on:touchend={handleTouchEnd}/>
