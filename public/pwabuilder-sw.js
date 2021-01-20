8feb8acad495d9b1f1330c77c1f6890dab191970
Azgaar <maxganiev@yandex.ru>
2021-01-20 22:29:44 +0300

    v0.8.12a - PWA: standalone looks better

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -1,7 +1,5 @@
-// This is the "Offline page" service worker
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js");
-const CACHE = "pwabuilder-page";
-const offlineFallbackPage = "offline.html";
+const CACHE = "armoria-cache";
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 self.addEventListener("message", event => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -9,30 +7,9 @@ self.addEventListener("message", event => {
   }
 });
 
-self.addEventListener("install", async event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.add(offlineFallbackPage)));
-});
-
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener("fetch", event => {
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      (async () => {
-        try {
-          const preloadResp = await event.preloadResponse;
-          if (preloadResp) return preloadResp;
-
-          const networkResp = await fetch(event.request);
-          return networkResp;
-        } catch (error) {
-          const cache = await caches.open(CACHE);
-          const cachedResp = await cache.match(offlineFallbackPage);
-          return cachedResp;
-        }
-      })()
-    );
-  }
-});
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.CacheFirst({
+    cacheName: CACHE
+  })
+);
