0d76ac14a1fd2a8e2847589626c65d6bf2c497f5
Azgaar <maxganiev@yandex.ru>
2021-01-20 20:32:08 +0300

    v0.8.05a - PWA 1st try

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import App from "./App.svelte";
+import "https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate";
+
+const el = document.createElement("pwa-update");
+document.body.appendChild(el);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('./pwabuilder-sw.js');
+  });
+}
 
 const app = new App({
   target: document.body,
