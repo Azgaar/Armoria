a1980d7d5c62c2c12289dacb68e173481cdd068b
Azgaar <maxganiev@yandex.ru>
2021-01-20 21:42:36 +0300

    v0.8.10a - PWA: fullscreen, offline page update

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
 import App from "./App.svelte";
 import "https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate";
 
-const el = document.createElement("pwa-update");
-document.body.appendChild(el);
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./pwabuilder-sw.js');
-  });
-}
+// register serviceWorker
+if ("serviceWorker" in navigator) window.addEventListener('load', () => navigator.serviceWorker.register('./pwabuilder-sw.js'));
 
 const app = new App({
   target: document.body,
