cec428b44f1294d22e80c58bbe46503f5a603aa6
Azgaar <maxganiev@yandex.ru>
2021-01-20 21:43:26 +0300

    v0.8.11a - PWA: remove update script

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import App from "./App.svelte";
-import "https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate";
 
 // register serviceWorker
 if ("serviceWorker" in navigator) window.addEventListener('load', () => navigator.serviceWorker.register('./pwabuilder-sw.js'));
