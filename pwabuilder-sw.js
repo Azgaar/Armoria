0cc92a5f028ba5c87c4cc728a56840e09e4736e7
Azgaar <maxganiev@yandex.ru>
2021-01-20 18:06:32 +0000

    Deploying to gh-pages from  @ 4550a90f6f2c2310f8ddd53cad3dbaef8c6d9750 🚀

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,9 +1,7 @@
 // This is the "Offline page" service worker
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js");
 const CACHE = "pwabuilder-page";
-
-// replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "index.html";
 
 self.addEventListener("message", event => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -25,10 +23,7 @@ self.addEventListener("fetch", event => {
       (async () => {
         try {
           const preloadResp = await event.preloadResponse;
-
-          if (preloadResp) {
-            return preloadResp;
-          }
+          if (preloadResp) return preloadResp;
 
           const networkResp = await fetch(event.request);
           return networkResp;
