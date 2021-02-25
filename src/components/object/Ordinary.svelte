d7df53364d107b4b254e84f5e5746d5a45d55eb6
Azgaar <maxganiev@yandex.ru>
2021-02-25 21:53:28 +0300

    ordinaries to work with transform

--- src/components/object/Ordinary.svelte
+++ src/components/object/Ordinary.svelte
@@ -16,15 +16,15 @@
 </script>
 
 {#if ordinary.ordinary === "bordure"}
-  <g class="ordinary" {i} transform={transform(ordinary)} transform-origin="center" on:mousedown={addDrag}>
+  <g class="ordinary" {i} transform={transform(ordinary)} on:mousedown={addDrag}>
     <path d={shieldPath} fill="none" stroke={t} stroke-width="16.7%"/>
   </g>
 {:else if ordinary.ordinary === "orle"}
-  <g class="ordinary" {i} transform={transform(ordinary)} transform-origin="center" on:mousedown={addDrag}>
-    <path d={shieldPath} fill="none" stroke={t} stroke-width="5%" transform="scale(.85)" transform-origin="center"/>
+  <g class="ordinary" {i} transform={transform(ordinary)} on:mousedown={addDrag}>
+    <path d={shieldPath} fill="none" stroke={t} stroke-width="5%" transform="translate(15 15) scale(.85)"/>
   </g>
 {:else}
-  <g class="ordinary" {i} transform={transform(ordinary)} transform-origin="center" fill={t} {stroke} stroke-width={width} on:mousedown={addDrag}>
+  <g class="ordinary" {i} transform={transform(ordinary)} fill={t} {stroke} stroke-width={width} on:mousedown={addDrag}>
     {@html getTemplate(ordinary.ordinary, ordinary.line)}
   </g>
 {/if}
