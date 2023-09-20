f392f173823751ee639f36b86327e90710de3035
Blipz <filinn.blip@gmail.com>
2023-09-20 14:39:35 +0200

    Use absolute values to work with zoom level

--- src/components/object/Ordinary.svelte
+++ src/components/object/Ordinary.svelte
@@ -17,9 +17,9 @@
 
 <g class="ordinary" {i} transform={transform(ordinary)} fill={t} {stroke} stroke-width={width} on:mousedown={addDrag}>
   {#if ordinary.ordinary === "bordure"}
-    <path d={shieldPath} fill="none" stroke={t} stroke-width="16.7%" />
+    <path d={shieldPath} fill="none" stroke={t} stroke-width="33.3" />
   {:else if ordinary.ordinary === "orle"}
-    <path d={shieldPath} fill="none" stroke={t} stroke-width="5%" transform="translate(15 15) scale(.85)" />
+    <path d={shieldPath} fill="none" stroke={t} stroke-width="10" transform="translate(15 15) scale(.85)" />
   {:else}
     <!-- Remove superfluous <g> for Svelte >= 3.48 (see https://github.com/sveltejs/svelte/issues/7450) -->
     <g>{@html getTemplate(ordinary.ordinary, ordinary.line)}</g>
