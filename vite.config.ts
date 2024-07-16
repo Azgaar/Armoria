66db45970a944b3ac9de7786838f2f62994e3534
Blipz <filinn.blip@gmail.com>
2024-07-16 18:53:16 +0200

    Adapt for PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	define: {
+		'process.env.NODE_ENV': '"production"'
+	}
 });
