3a436a02af3a87ff2e551dad640e680587e46b2c
Blipz <filinn.blip@gmail.com>
2024-07-25 02:24:36 +0200

    Suppress a11y warnings for now

--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,11 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
+	},
+
+	onwarn: (warning, handler) => {
+		if (warning.code === "a11y-click-events-have-key-events" || warning.code === "a11y-no-static-element-interactions") return;
+		handler(warning);
 	}
 };
 
