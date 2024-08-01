8004711eabb895503060705307ef8319dba36731
Azgaar <azgaar.fmg@yandex.com>
2024-08-01 19:14:33 +0200

    feat: adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-vercel";
+import {vitePreprocess} from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	},
-
-	onwarn: (warning, handler) => {
-		if (warning.code === "a11y-click-events-have-key-events" || warning.code === "a11y-no-static-element-interactions") return;
-		handler(warning);
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter()
+  },
+  onwarn: (warning, handler) => {
+    if (warning.code === "a11y-click-events-have-key-events" || warning.code === "a11y-no-static-element-interactions") return;
+    handler(warning);
+  }
 };
 
 export default config;
